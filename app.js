5bc7c2e0861fac7e3f2bf043326b842d2a250433
malee1 <46596915+malee1@users.noreply.github.com>
2020-12-01 18:54:55 +0000

    getcustomer server and mock customer data added

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const cors = require('cors');
 const app = express();
 const port = 3002;
 
+const customer = require('./data/mockCustomer');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send({
         name: 'From Server Hello World'
     });
+});
+
+app.get('/getcustomer', (req, res) => {
+    res.send(customer);
 });
 
 app.listen(port, () => {
---
+++ data/mockCustomer.js
@@ -0,0 +1,16 @@
+const customer1 = {
+  name: 'Serv Joe Bloggs',
+  dob: 'Serv 12345',
+  uRef: 'Serv 67890',
+  tel: 'Serv 123456789',
+  timeWait: '10 mins',
+  context: 'I have a query',
+  recording: 'Inactive',
+  callInfo: 'ABCDE',
+  alerts: 'Important Info 1',
+  service: 'Alert1',
+  keyInfo: 'Info 1',
+  auth: 'Approved'
+};
+
+module.exports = customer1;
